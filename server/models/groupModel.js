6d2e230359d7996f2bb9202a192c41797f3a7937
The Legendary John <legendaryjohn2005@gmail.com>
2024-09-06 12:24:14 +0700

    Revert "Implementing Group (unfinished)"
    This reverts commit ceaca667c913395029cd7bbd0a6a32e6dec763d4.

--- server/models/groupModel.js
+++ server/models/groupModel.js
@@ -3,24 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const groupModel = new Schema(
-	{
-		name: { type: String, required: true },
-		admin: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: "User",
-				default: [],
-			},
-		],
-		members: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: "User",
-				default: [],
-			},
-		],
-	},
-	{ timestamps: true }
+    {
+        name: { type: String, required: true },
+        members: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+    },
+    { timestamps: true }
 );
 
-module.exports = mongoose.model("Group", groupModel);
+module.exports = mongoose.model("Group", groupModel)
